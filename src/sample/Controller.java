2033699fcfb636f5f0102fc07b3f81407ae450f7
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-15 21:36:50 +0300

    clientGUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField msgField;
+
+    public void sendMessage(ActionEvent actionEvent) {
+        if (msgField.getText().trim().length() > 0) {
+            textArea.appendText(msgField.getText() + "\n");
+            msgField.clear();
+            msgField.requestFocus();
+        }
+    }
 }
